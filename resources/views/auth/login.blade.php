8ab9f12ed15f7bdd13021c12396ffecb733ef094
ricky24m <168514470+ricky24m@users.noreply.github.com>
2024-04-30 13:44:07 +0700

    title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="theme-color" content="#000000">
-    <title>E-Presensi Kerjoo </title>
+    <title>E-Presensi kerjoo.lee </title>
     <meta name="description" content="Mobilekit HTML Mobile UI Kit">
     <meta name="keywords" content="bootstrap 4, mobile template, cordova, phonegap, mobile, html" />
     <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png')}}" sizes="32x32">
